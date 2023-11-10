f4582366b991bbd57ed1b143b4542267e9c54673
Paulo Carvalho <prferrazc@gmail.com>
2023-11-10 17:56:10 -0300

    classe campo e celula aturalizadas

--- CampoMinadoLPOO/src/teste/Campo.java
+++ CampoMinadoLPOO/src/teste/Campo.java
@@ -5,38 +5,39 @@ import java.security.SecureRandom;
 
 public class Campo {
 	
+	
+	private int linha = 7;
+	private int coluna = 7;
+
 	public void imprimeMatriz() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o tamanho da matriz");
-        final int tamanhoMatriz = sc.nextInt();
-        sc.close();
-
-        char[][] matriz = gerarMatriz(tamanhoMatriz);
+       
+   
+        char[][] matriz = gerarMatriz(linha, coluna);
 
 
-        for (int l = 0; l < tamanhoMatriz; l++) {
-            for (int c = 0; c < tamanhoMatriz; c++) {
+        for (int l = 0; l < linha; l++) {
+            for (int c = 0; c < coluna; c++) {
                 System.out.print(matriz[l][c] + " ");
             }
             System.out.println(); 
         }
     }
 
-    public static char[][] gerarMatriz(int tamanhoMatriz) {
-        char[][] matriz = new char[tamanhoMatriz][tamanhoMatriz];
+    public static char[][] gerarMatriz(int linha , int coluna) {
+        char[][] matriz = new char[linha][coluna];
         SecureRandom random = new SecureRandom();
 
-        for (int l = 0; l < tamanhoMatriz; l++) {
-            for (int c = 0; c < tamanhoMatriz; c++) {
+        for (int l = 0; l < linha; l++) {
+            for (int c = 0; c < coluna; c++) {
             	 if (random.nextInt(4) == 0) {
             		 Celula colocaBomba = new Celula();
             		 colocaBomba.checarBomba();
             		 colocaBomba.setBomba(true);
             		 boolean verifica = colocaBomba.getBomba();
-            		 matriz[l][c] = '-'; // Caractere '*' em posições randômicas
+            		 matriz[l][c] = '-'; 
             		 System.out.println("A bomba foi colocada: " + verifica);
                  } else {
-                     matriz[l][c] = (char) ('-'); // Número aleatório de 0 a 3
+                     matriz[l][c] = (char) ('-'); 
                  }
              }
          }
